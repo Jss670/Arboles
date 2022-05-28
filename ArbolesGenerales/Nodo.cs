139fd8356472724835d643b2ff9b483def005cab
Octavio Garcia <jss670@gmail.com>
2022-05-28 12:59:13 -0700

    clase nodo

---
+++ ArbolesGenerales/Nodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class Nodo
+    {
+        public string Dato { get; set; } //prop 2xtab
+        public Nodo Hijo { get; set; }
+
+        public Nodo Hermano { get; set; }
+
+        public Nodo(string dato, Nodo hijo = null, Nodo hermano = null) //ctor tab tab
+        {
+            Dato = dato;
+            Hijo = hijo;
+            Hermano = hermano;
+
+        }
+
+    }
+}
