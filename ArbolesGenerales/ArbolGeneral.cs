802a23fe8fe7f8b421c79eee971780ec774a7e78
Octavio Garcia <jss670@gmail.com>
2022-05-28 13:28:10 -0700

    asdasdas

---
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    Public class ArbolGeneral
+    {
+        private readonly Nodo raiz;
+
+        //public Nodo Raiz => raiz;
+
+        public Nodo Raiz { get { return raiz } }
+
+        public ArbolGeneral(string dato)
+        {
+            raiz = new Nodo(dato);
+        }
+
+        public Nodo InsertarHijo(Nodo padre, string dato)
+        {
+            if (string.IsNullOrWhiteSpace(dato)
+            {
+                throw new Exception("el dato esta vacio");
+
+            }
+            if (padre is null)
+            {
+                throw new Exception("el padre no puede ser nulo");
+            }
+            if (padre.Hijo is null)
+            {
+                padre.Hijo = new Nodo(dato);
+                return padre.Hijo;
+
+            }
+        }
+    }
+}
